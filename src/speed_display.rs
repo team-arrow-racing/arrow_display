df85cf636e5e04a2e8556985bbbc1a4eb618ce85
LaraClara <clara@youpay.ai>
2023-08-14 20:51:25 +1000

    style: Add km/h text

--- src/speed_display.rs
+++ src/speed_display.rs
@@ -31,10 +31,10 @@ where
 
         let text_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
 
-        let binding = speed.to_string();
+        // Add km/h to the speed
+        let binding = speed.to_string() + " km/h";
 
-        // let speed_text = Text::new(&binding, Point::new(0, 0), text_style);
-        let speed_text = Text::new(&binding, Point::new(15, 15), text_style);
+        let speed_text = Text::new(&binding, Point::new(15, 28), text_style);
 
 
         Rectangle::new(Point::new(0, 0), Size::new(64, 64))
