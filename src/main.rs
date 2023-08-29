5e8054c1a09b740262af824f79a79d75fff29afe
damien-raju <103402521+damien-raju@users.noreply.github.com>
2023-08-29 20:09:34 +1000

    hotfix: Demo progress

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,41 @@
 //! cortex-m-rtic example
-//! Tested on BlackPill dev board with stm32f411ceu microcontroller
+//! Tested on BlackPill dev board with stm32l411ceu microcontroller
 //! The LCD RESET pin was hard puled to Vcc therefore
 //! DummyOutputPin was used as the reset pin
 
 #![no_main]
 #![no_std]
 
-#[rtic::app(device = stm32f4xx_hal::pac)]
+use defmt_rtt as _;
+use panic_probe as _;
+
+#[rtic::app(device = stm32l4xx_hal::pac)]
 mod app {
     use display_interface_spi::SPIInterface;
+    use embedded_hal::spi::{Mode, Phase, Polarity};
     use embedded_graphics::{
         mono_font::{ascii::FONT_6X10, MonoTextStyle},
         pixelcolor::Rgb565,
         prelude::*,
         text::{Alignment, Text},
     };
-    use embedded_hal::digital::{blocking::OutputPin, ErrorType, PinState};
+
     use ili9341::{DisplaySize240x320, Ili9341, Orientation};
-    use stm32f4xx_hal::{
+    use stm32l4xx_hal::{
+        self,
+        delay::DelayCM,
         prelude::*,
-        spi::{Mode, NoMiso, Phase, Polarity},
-        timer::Channel,
+        spi::Spi,
+        gpio::{Alternate, Output, PushPull}, 
+    };
+
+    /// SPI mode
+    pub const MODE: Mode = Mode {
+        phase: Phase::CaptureOnFirstTransition,
+        polarity: Polarity::IdleLow,
     };
 
     #[derive(Default)]
-    pub struct DummyOutputPin;
-    impl ErrorType for DummyOutputPin {
-        type Error = ();
-    }
-
-    impl OutputPin for DummyOutputPin {
-        fn set_low(&mut self) -> Result<(), Self::Error> {
-            Ok(())
-        }
-        fn set_high(&mut self) -> Result<(), Self::Error> {
-            Ok(())
-        }
-        fn set_state(&mut self, _state: PinState) -> Result<(), Self::Error> {
-            Ok(())
-        }
-    }
 
     #[shared]
     struct Shared {}
@@ -48,33 +44,67 @@ mod app {
     struct Local {}
 
     #[init]
-    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
-        let dp = ctx.device;
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        let dp = cx.device;
 
-        let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.use_hse(25.MHz()).sysclk(100.MHz()).freeze();
+        let mut flash = cx.device.FLASH.constrain();
+        let mut rcc = cx.device.RCC.constrain();
+        let mut pwr = cx.device.PWR.constrain(&mut rcc.apb1r1);
+        let mut gpioa = cx.device.GPIOA.split(&mut rcc.ahb2);
+        let mut gpiob = cx.device.GPIOB.split(&mut rcc.ahb2);
 
-        let gpioa = dp.GPIOA.split();
-        let gpiob = dp.GPIOB.split();
+        let clocks = rcc.cfgr.sysclk(80.MHz()).freeze(&mut flash.acr, &mut pwr);
 
         /*
          *  The ILI9341 driver
          */
-        let lcd_clk = gpiob.pb0.into_alternate();
-        let lcd_miso = NoMiso {};
-        let lcd_mosi: stm32f4xx_hal::gpio::Pin<'A', 10, stm32f4xx_hal::gpio::Alternate<6>> = gpioa.pa10.into_alternate().internal_pull_up(true);
-        let lcd_dc = gpiob.pb1.into_push_pull_output();
-        let lcd_cs = gpiob.pb2.into_push_pull_output();
-        let mode = Mode {
-            polarity: Polarity::IdleLow,
-            phase: Phase::CaptureOnFirstTransition,
-        };
-        let lcd_spi = dp
-            .SPI5
-            .spi((lcd_clk, lcd_miso, lcd_mosi), mode, 2.MHz(), &clocks);
+        // Configure SPI
+        let lcd_clk = gpiob.pb13.into_alternate(
+            &mut gpiob.moder,
+            &mut gpiob.otyper,
+            &mut gpiob.afrh,
+        );
+
+        let lcd_miso = gpiob.pb14.into_alternate(
+            &mut gpiob.moder,
+            &mut gpiob.otyper,
+            &mut gpiob.afrh,
+        );
+
+        let lcd_mosi = gpiob.pb15.into_alternate(
+            &mut gpiob.moder,
+            &mut gpiob.otyper,
+            &mut gpiob.afrh,
+        );
+
+        let lcd_cs = gpiob
+            .pb12
+            .into_open_drain_output(&mut gpiob.moder, &mut gpiob.otyper);
+
+        let lcd_spi = Spi::spi2(
+            cx.device.SPI2,
+            (lcd_clk, lcd_miso, lcd_mosi),
+            MODE,
+            16.MHz(),
+            clocks,
+            &mut rcc.apb1r1,
+        );
+        
+        // let lcd_clk = gpiob.pb0.into_alternate();
+        // let lcd_miso = NoMiso {};
+        // let lcd_mosi: stm32l4xx_hal::gpio::Pin<'A', 10, stm32l4xx_hal::gpio::Alternate<6>> = gpioa.pa10.into_alternate().internal_pull_up(true);
+        let lcd_dc = gpiob.pb1.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+        // let lcd_cs = gpiob.pb2.into_push_pull_output();
+        // let mode = Mode {
+        //     polarity: Polarity::IdleLow,
+        //     phase: Phase::CaptureOnFirstTransition,
+        // };
+        // let lcd_spi = dp
+        //     .SPI5
+        //     .spi((lcd_clk, lcd_miso, lcd_mosi), mode, 2.MHz(), &clocks);
         let spi_iface = SPIInterface::new(lcd_spi, lcd_dc, lcd_cs);
-        let dummy_reset = DummyOutputPin::default();
-        let mut delay = dp.TIM1.delay_us(&clocks);
+        let dummy_reset = gpiob.pb3.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+        let mut delay = DelayCM::new(clocks);
         let mut lcd = Ili9341::new(
             spi_iface,
             dummy_reset,
@@ -89,7 +119,7 @@ mod app {
 
         // Create a text at position (20, 30) and draw it using the previously defined style
         Text::with_alignment(
-            "First line\nSecond line",
+            "First line",
             Point::new(20, 30),
             style,
             Alignment::Center,
@@ -106,4 +136,9 @@ mod app {
             cortex_m::asm::nop();
         }
     }
+}
+
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
 }
